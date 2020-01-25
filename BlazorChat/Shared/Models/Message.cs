01b8d9519f760ef93f48377b4adeb5cc487d0227
Andy <andystav91@gmail.com>
2020-01-25 00:44:42 +0000

    Adding Initial Models

---
+++ BlazorChat/Shared/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorChat.Shared.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string MessageContent { get; set; }  
+        public User Sender { get; set; }
+        public User Receiver { get; set;  }
+
+    }
+}
---
+++ BlazorChat/Shared/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorChat.Shared.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public virtual List<Message> Messages { get; set;  }
+
+    }
+}
